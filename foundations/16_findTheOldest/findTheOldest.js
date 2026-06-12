4911564a360fa1ddfc2faf8eff56008436fd7a3c
mindmazing <67432331+Mindmazing@users.noreply.github.com>
2026-06-11 19:23:26 -0600

    Finish foundations js section

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  return array.reduce(
+    (accum, current) => {
+      let accumAge = accum.yearOfDeath - accum.yearOfBirth;
+      let yearOfDeath = current.yearOfDeath
+        ? current.yearOfDeath
+        : new Date().getFullYear();
+      let curreAge = yearOfDeath - current.yearOfBirth;
 
+      if (curreAge > accumAge) {
+        accum.name = current.name;
+        accum.yearOfDeath = current.yearOfDeath
+          ? current.yearOfDeath
+          : yearOfDeath;
+        accum.yearOfBirth = current.yearOfBirth;
+        console.log(accum);
+      }
+      return accum;
+    },
+    {
+      name: "",
+      yearOfBirth: 0,
+      yearOfDeath: 0,
+    },
+  );
 };
 
 // Do not edit below this line
